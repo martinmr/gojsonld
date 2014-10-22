197fb6c29e7346d54f5a05919a617671e9fe7b25
Martin Martinez Rivera <martinmr@mit.edu>
2014-10-21 21:48:23 -0400

    Modified resolve to handle nil values better

--- urlUtils.go
+++ urlUtils.go
@@ -3,19 +3,32 @@ package gojsonld
 import (
 	"net/url"
 	"path/filepath"
+	"strings"
 )
 
-func resolve(base, ref string) (string, error) {
-	baseUrl, baseErr := url.Parse(base)
-	refUrl, refErr := url.Parse(ref)
-	if baseErr != nil {
-		return "", baseErr
+func resolve(base, ref *string) (*string, error) {
+	if isNil(base) {
+		if isNil(ref) {
+			return nil, nil
+		}
+		returnValue := *ref
+		return &returnValue, nil
 	}
-	if refErr != nil {
-		return "", refErr
+	if isNil(ref) || strings.Trim(*base, "") == "" {
+		returnValue := *base
+		return &returnValue, nil
+	}
+	baseUrl, baseErr := url.Parse(*base)
+	refUrl, refErr := url.Parse(*ref)
+	if !isNil(baseErr) {
+		return nil, baseErr
+	}
+	if !isNil(refErr) {
+		return nil, refErr
 	}
 	resolvedUrl := baseUrl.ResolveReference(refUrl)
-	return resolvedUrl.String(), nil
+	returnValue := resolvedUrl.String()
+	return &returnValue, nil
 }
 
 func removeBase(base, ref string) (string, error) {
