8c39d57eee7823c18f72fc85bf55db526836cad5
Martin Martinez Rivera <martinmr@mit.edu>
2014-11-05 21:59:48 -0500

    added RDF dataset

--- rdfDataset.go
+++ rdfDataset.go
@@ -1,1 +1,12 @@
 package gojsonld
+
+type Dataset struct {
+	Graphs map[string][]*Triple
+}
+
+func NewDataset() *Dataset {
+	dataset := &Dataset{}
+	dataset.Graphs = make(map[string][]*Triple, 0)
+	dataset.Graphs["@default"] = make([]*Triple, 0)
+	return dataset
+}
